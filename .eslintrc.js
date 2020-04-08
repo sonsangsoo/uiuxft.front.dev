eb2e0d4fcce1757d2ebfce066e20002e17fd3ac9
Junseopsan <metternichch@gmail.com>
2020-04-08 17:11:52 +0900

    import/no-relative-parent-imports 'OFF'

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     'no-console': process.env.PRE_COMMIT
       ? ['error', { allow: ['warn', 'error'] }]
       : 'off',
-    'import/no-relative-parent-imports': 'error',
+    'import/no-relative-parent-imports': 'off',
     'import/order': 'error',
     'vue/array-bracket-spacing': 'error',
     'vue/arrow-spacing': 'error',
