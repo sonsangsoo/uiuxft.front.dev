84d468bf6b3fbaa08595508a5763c8268a1340d1
sonsangsoo <vvdg25@naver.com>
2020-04-08 17:55:44 +0900

    commit son

---
+++ src/components/common/PostSearch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="content_sec">
+    <label for="" class="label_ty"
+      >우편번호<span class="c_point">*</span></label
+    >
+    <div class="half_wrap ty02">
+      <div><input type="text" class="input_ty"/></div>
+      <div><a href="#" class="btn_ty ty01">조회</a></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {}
+</script>
+
+<style lang="scss" module>
+@import '@design';
+</style>
---
+++ src/components/common/RealNameCheck.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="content_sec">
+    <label for="" class="label_ty"
+      >주민등록번호<span class="c_point">*</span></label
+    >
+    <div class="half_wrap ty02">
+      <div><input type="text" class="input_ty"/></div>
+      <div><a href="#" class="btn_ty ty01">성명정보조회</a></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {}
+</script>
+
+<style lang="scss" module>
+@import '@design';
+</style>
